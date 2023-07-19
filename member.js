62abd8fcb1eed12010d73cf5d53042f8c6971445
YRobles <yolanda_robles_ales@hotmail.com>
2023-07-19 17:50:46 +0000

    Copilot Second Commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    scope: {
+      member: '='
+    },
+    controller: function($scope, $element, $attrs) {
+      $scope.member = $scope.member || {};
+      $scope.member.skills = $scope.member.skills || [];
+      $scope.addSkill = function() {
+        $scope.member.skills.push({
+          name: '',
+          level: 0
+        });
+      };
+      $scope.removeSkill = function(index) {
+        $scope.member.skills.splice(index, 1);
+      };
+    }
+  };
+}
